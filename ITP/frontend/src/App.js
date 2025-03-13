4075b2b91d41de4ef61930f3a2987f9777935fbe
YMSenanayake <167961449+YMSenanayake@users.noreply.github.com>
2025-03-14 00:33:13 +0530

    login

--- ITP/frontend/src/App.js
+++ ITP/frontend/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/login/index";
 
 function App() {
   return (
-    <div>
-      <h1 className='bg-blue-400 '> ITP PROJECT</h1>
+    <div className='flex w-full h-screen'>
+      <div className="w-full flex items-center justify-center lg:w-1/2">
+         <Form />
+      </div>
+      <div className="hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
+        <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full"></div>
+      </div>
     </div>
   );
 }
---
+++ ITP/frontend/src/components/login/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function Form() {
+    return (
+      <div>
+        hello login
+      </div>
+    )
+  }
+
