acd7d64e9a93b829cad186182ac1f6ed4de7678d
Punara Seniduni <punaraseniduni@gmail.com>
2025-03-25 21:04:40 +0530

    Nupdate

--- ITP/frontend/src/App.js
+++ ITP/frontend/src/App.js
@@ -1,9 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import toast from "react-hot-toast";
+import "react-toastify/dist/ReactToastify.css";
+import React from "react";
+
+import { Toaster } from "react-hot-toast";
+import C_displayitem from "././page/order/C_displayitem";
+import ShoppingCart from "././page/order/ShoppingCart";
+import PaymentPage from "././page/order/payment";
+import CheckoutPage from "././page/order/checkout";
+
+
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <BrowserRouter>
+        <Toaster position="top-center" reverseOrder={false} />
+
+     <Routes>
+     <Route path="/c_displayitem" element={<C_displayitem/>}/>
+     <Route path="shoppingCart" element={<ShoppingCart/>}/>
+      <Route path="checkout" element={<CheckoutPage/>}/>
+      <Route path="payment" element={<PaymentPage/>}/>
+
+
+
+     </Routes>
+     </BrowserRouter>
      
     </div>
   );
