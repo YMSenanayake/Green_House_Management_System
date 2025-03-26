8bc2224bbe8277837dea60c8faf1a1b6c82ee9be
ThevinduPesara <thevindupesara@gmail.com>
2025-03-26 11:38:09 +0530

    update

--- ITP/frontend/src/App.js
+++ ITP/frontend/src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import React from "react";
+
+import Machine from "./page/machine/Machine";
+import {MachineUpdate} from "./page/machine/MachineUpdate";
+import {MachinePdf} from "./page/machine/MachinePdf";
+import {MVehicle} from "./page/machine/MVehicle";
+import Machinedashboard from "./page/machine/Machinedashboard";
+
+
 
 function App() {
   return (
-    <div>
-      <h1 className='bg-blue-400 '> ITP PROJECT</h1>
+    <div className="App">
+      <BrowserRouter>
+        <ToastContainer position="top-center" autoClose={3000} />
+        <Routes>
+          <Route path="/m_machine" element={<Machine />} />
+          <Route path="/m_update/:mid" element={<MachineUpdate />} />
+          <Route path="/m_MachinePdf" element={<MachinePdf />} />
+          <Route path="/m_MVehicle" element={<MVehicle />} />
+          <Route path="/m_machinedashboard" element={<Machinedashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ ITP/frontend/src/components/home/Home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Green House Management System</h1>
+      <p>This is the home page where you can navigate to different sections of the app.</p>
+    </div>
+  );
+};
+
+export default Home;
