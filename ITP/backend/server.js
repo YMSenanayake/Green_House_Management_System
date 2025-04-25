63c07c8131b0a51ca9416a25203e53a7377dd9b1
YMSenanayake <167961449+YMSenanayake@users.noreply.github.com>
2025-04-25 19:04:55 +0530

    Update server.js

--- ITP/backend/server.js
+++ ITP/backend/server.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on: http://localhost:${port}`);
 });
