9452ab83c693711c4fb7be30cedcdf2fa253ca3a
YMSenanayake <167961449+YMSenanayake@users.noreply.github.com>
2025-04-25 19:12:31 +0530

    update

--- ITP/frontend/src/components/home/index.js
+++ ITP/frontend/src/components/home/index.js
@@ -362,7 +362,7 @@ function Homepage() {
                     <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                     </svg>
-                    GreenGrow@gmail.com
+                    GreenGrow891@gmail.com
                   </li>
                   <li className="flex items-center gap-3">
                     <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
