d2af129af315b39f7d8d08ec4c9802cb9d831635
YMSenanayake <167961449+YMSenanayake@users.noreply.github.com>
2025-04-26 22:00:31 +0530

    fixed

--- ITP/frontend/src/page/financial/components/Header.jsx
+++ ITP/frontend/src/page/financial/components/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
           {showDropdown && (
             <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50">
               <Link 
-                to="/profile" 
+                to="/e_profile_dashboard" 
                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-green-100"
                 onClick={() => setShowDropdown(false)}
               >
